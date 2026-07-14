26b4341fd17a01f1e18da2de7bc4588e76e77838
nghiacollab <tuannghia.tech@gmail.com>
2026-07-14 11:27:11 +0700

    fix: add env library to load variable

--- src/main/java/com/abp/api_billing_platform/ApiBillingPlatformApplication.java
+++ src/main/java/com/abp/api_billing_platform/ApiBillingPlatformApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class ApiBillingPlatformApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(ApiBillingPlatformApplication.class, args);
 	}
 
